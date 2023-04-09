c4b52141759a9641c70e71fb4d7ee280ac0393e2
ArseniyChaldaev <arsenii.chaldaev@gmail.com>
2023-04-09 22:59:39 +0300

    полностью переписал код. Ввел 1 уровень ниже time set'а. А на выдачу задания поставил заглушку.

--- main.py
+++ main.py
@@ -1,20 +1,57 @@
 import telebot
+from telebot import types
 
-bot = telebot.TeleBot('6150776858:AAFgMzCcTuLUDnEoO8RNS2HMpNkH5xaf22Q')
+TOKEN = '6150776858:AAFgMzCcTuLUDnEoO8RNS2HMpNkH5xaf22Q'
+
+bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет! Выбери, что ты хочешь сделать:',
-                     reply_markup=main_keyboard())
+def start(message):
+    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+
+    btn_give_task = types.KeyboardButton("give me a task")
+    btn_time_change = types.KeyboardButton("change the generation time")
+
+    markup.add(btn_give_task, btn_time_change)
+
+    bot.send_message(message.chat.id, 'Hi, {0.first_name}! Choose what you want to do:'.format(message.from_user),
+                     reply_markup=markup)
 
 
-def main_keyboard():
-    markup = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    btn_task = telebot.types.KeyboardButton("Выдача задания")
-    btn_time = telebot.types.KeyboardButton("Регулировка времени генерации")
-    markup.add(btn_task, btn_time)
-    return markup
+@bot.message_handler(content_types=['text'])
+def bot_message(message):
+    if message.chat.type == 'private':
+        if message.text == 'give me a task':
+            bot.send_message(message.chat.id, 'ЗАГЛУШКА')
+
+        elif message.text == 'change the generation time':
+
+            markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+
+            btn_time_type_1 = types.KeyboardButton('10:00')
+            btn_time_type_2 = types.KeyboardButton('20:00')
+            back = types.KeyboardButton('Back')
+
+            markup.add(btn_time_type_1, btn_time_type_2, back)
+
+            bot.send_message(message.chat.id,
+                             'Choose the right time:'.format(message.from_user),
+                             reply_markup=markup)
+
+        elif message.text == 'Back' or message.text == '10:00' or message.text == '20:00':
+            if message.text == '10:00' or message.text == '20:00':
+                bot.send_message(message.chat.id, 'The time is setted.')
+            markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+
+            btn_give_task = types.KeyboardButton("give me a task")
+            btn_time_change = types.KeyboardButton("change the generation time")
+
+            markup.add(btn_give_task, btn_time_change)
+
+            bot.send_message(message.chat.id,
+                             '{0.first_name}, choose what you want to do:'.format(message.from_user),
+                             reply_markup=markup)
 
 
 bot.polling(none_stop=True)
